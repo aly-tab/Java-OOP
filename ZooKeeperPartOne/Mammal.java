846789c44e7cfca0e2efd6e3241284fc753e1f31
w-phil <wphillips892@gmail.com>
2021-08-03 13:52:57 -0400

    ZooKeeperPartOne

---
+++ ZooKeeperPartOne/Mammal.java
@@ -0,0 +1,11 @@
+public class Mammal {
+    public int energyLevel;
+
+    public void setEnergyLevel(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+
+    public String displayEnergy(int energyLevel) {
+        return "Energy level: " + this.energyLevel;
+    }
+}
---
+++ ZooKeeperPartOne/ZooKeeperTest.java
@@ -0,0 +1,12 @@
+public class ZooKeeperTest {
+    public static void main(String[] args) {
+        Gorilla gorilla = new Gorilla();
+        gorilla.setEnergyLevel(50);
+        System.out.println(gorilla.throwSomething());
+        System.out.println(gorilla.throwSomething());
+        System.out.println(gorilla.throwSomething());
+        System.out.println(gorilla.eatBananas());
+        System.out.println(gorilla.eatBananas());
+        System.out.println(gorilla.climb());
+    }
+}
