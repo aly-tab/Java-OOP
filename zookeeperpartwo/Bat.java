83f107e6556b112c74d9a27ada77d999b0d599f4
w-phil <wphillips892@gmail.com>
2021-08-03 15:23:02 -0400

    zookeeperpartwo

---
+++ zookeeperpartwo/Bat.java
@@ -0,0 +1,18 @@
+package com.codingdojo.zookeeperpartwo;
+
+public class Bat extends Mammal {
+    public String fly() {
+        energyLevel = energyLevel - 50;
+        return "ZOOORPPPPPP! Energy level: " + this.energyLevel;
+    }
+
+    public String eatHumans() {
+        energyLevel = energyLevel + 25;
+        return "Noms! Energy level: " + this.energyLevel;
+    }
+
+    public String attackTown() {
+        energyLevel = energyLevel - 100;
+        return "Murdurdur! Energy level: " + this.energyLevel;
+    }  
+}
